1f02abdd6496ae3beec6bcbd801edbeb43d2cef9
holmr <holmr@rpi.edu>
2017-05-18 13:51:31 -0400

    Working on AUTH_USER_MODEL

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -9,7 +10,9 @@ from django.db import models
 from django.shortcuts import render
 from django.utils import timezone
 
+
 from . import imglib
+
 
 
 
@@ -19,7 +22,7 @@ class UserManager(BaseUserManager):
             raise ValueError("Users must have an email address")
         if not display_name:
             display_name = username
-        if not User.objects.filter(username__iexact=username).exists():
+        if not User.objects.filter(user__username__iexact=username).exists():
             user = self.model(
                 email=self.normalize_email(email),
                 username=username,
@@ -57,21 +60,17 @@ class FileField(models.FileField):
         super(FileField, self).save_form_data(instance, data)
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=40, unique=True)
+class User(PermissionsMixin):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     display_name = models.CharField(max_length=140)
     bio = models.CharField(max_length=140, blank=True, default="")
     avatar = FileField(blank=True, null=True,upload_to='avatar')
     date_joined = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    # from CSDT:
-    gender = models.CharField(max_length=100, null=True, blank=True)
-    race = models.CharField(max_length=100, null=True, blank=True)
-    age = models.PositiveIntegerField(null=True, blank=True)
-    first_name = models.CharField(max_length=100, null=True, blank=True)
-    last_name = models.CharField(max_length=100, null=True, blank=True)
     objects = UserManager()
     
     USERNAME_FIELD = "email"
